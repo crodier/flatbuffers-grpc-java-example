780b0b92319316c3a989a47978dd1dfd0692a6f5
chris <chris@newtails.io>
2023-05-01 22:46:51 -0400

    Says it is pinning to Cores now!

--- grpc-server/src/main/java/com/example/fbgrpc/PinnedCoreAffinityThreadFactory.java
+++ grpc-server/src/main/java/com/example/fbgrpc/PinnedCoreAffinityThreadFactory.java
@@ -1,23 +1,26 @@
 package com.example.fbgrpc;
 
 import net.openhft.affinity.AffinityLock;
+import net.openhft.affinity.AffinityStrategy;
 
 import java.util.concurrent.ThreadFactory;
 
 public class PinnedCoreAffinityThreadFactory implements ThreadFactory {
 
+    private final int[] cpus;
     String name = "pinned_";
     private final int core;
 
     public PinnedCoreAffinityThreadFactory(String name, int core) {
         this.name = name;
         this.core = core;
+        this.cpus = new int[]{core};
     }
 
     @Override
     public Thread newThread(Runnable r) {
         Thread t = new Thread(() -> {
-            try (AffinityLock lock = AffinityLock.acquireLock(core)) {
+            try (AffinityLock lock = AffinityLock.acquireLock(cpus)) {
                 r.run();
             }
         }, name);
